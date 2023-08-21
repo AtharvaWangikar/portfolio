13a81a1194284f1ed8cf1b0712e50e88f56e7211
Atharva Wangikar <wangikarav1@gmail.com>
2023-08-21 22:54:37 +0530

    commit
    NextJS website

---
+++ components/AboutSection.tsx
@@ -0,0 +1,128 @@
+import React from "react"
+const Gskills = [
+  { skill: "Data Analysis" },
+  { skill: "Data Visualization" },
+  { skill: "Web Development" },
+  { skill: "Teamwork" },
+  { skill: "Adaptability" },
+  { skill: "Agile" },
+]
+
+const Tskills = [
+  { skill: "Python" },
+  { skill: "SQL" },
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "C/C++" },
+  { skill: "Java" },
+]
+const Toskills = [
+  { skill: "Spreadsheets" },
+  { skill: "MySQL/Postgres/SSMS" },
+  { skill: "MongoDB" },
+  { skill: "Tableau" },
+  { skill: "PowerBI" },
+  { skill: "Google Cloud" },
+  { skill: "Netlify" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "Tailwind CSS" },
+  { skill: "Git" },
+  { skill: "Vercel" },
+  { skill: "Jupyter Notebooks" },
+]
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-cyan-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is Atharva and I am a{" "}
+              <span className="font-bold">{"highly ambitious"}</span>,
+              <span className="font-bold">{" self-motivated"}</span>, and
+              <span className="font-bold">{" driven"}</span> software engineer
+              based in Pune.
+            </p>
+            <br />
+            <p>
+              I will graduate from Dr D Y Patil Institute of Technology, Pune  in 2024 with a BE in Computer Engineering.
+            </p>
+            <br />
+            <p>
+              I have a wide range of hobbies and passions that keep me busy.
+              From reading, riding, traveling, listening to music, watching TV, exploring and staying updated on Science & Technology.
+              I am always seeking new experiences and love to keep myself
+              engaged and learning new things.
+            </p>
+            <br />
+            <p>
+              I believe that you should{" "}
+              <span className="font-bold text-cyan-500">
+                never stop growing
+              </span>{" "}
+              and that&#39;s what I strive to do, I have a passion for
+              technology and a desire to always push the limits of what is
+              possible. I am excited to see where my career takes me and am
+              always open to new opportunities.
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {Gskills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-red-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <h1 className="text-2xl font-bold mb-6">Programming Languages</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {Tskills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-amber-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <h1 className="text-2xl font-bold mb-6">Tools & Technologies </h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {Toskills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-emerald-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
---
+++ components/ProjectsSection.tsx
@@ -0,0 +1,97 @@
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import SlideUp from "./SlideUp"
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+
+const projects = [
+  {
+    name: "IPL Analysis Dashboard",
+    description:
+      "Comprehensive visualization tool for analyzing Indian Premier League matches, team and player statistics. Focuses on KPIs such as team performance, player performance, season records by teams,etc.",
+    image: "/ipl.png",
+    github: "https://github.com/AtharvaWangikar?=repositories",
+    link: "/",
+  },
+  {
+    name: "COVID 19 Exploratory Analysis",
+    description: "•	Processed the comprehensive CoVID 19 dataset, and uncovered valuable conclusions and insights about the COVID-19 pandemic.",
+    image: "/cov.png",
+    github: "https://github.com/AtharvaWangikar/Analysis_of_COVID19",
+    link: "/",
+  },
+  {
+    name: "Netflix Clone using NextJS",
+    description:
+      "Full Stack project using React for frontend, Next for Server-side rendering, TailwindCSS for styling, Prisma for data Abstraction, MongoDB for storage, Next-Auth for Authentication, Typescript for scripting and Vercel for Deployment.",
+    image: "/nf.png",
+    github: "https://github.com/AtharvaWangikar/netflix-clone",
+    link: "https://netflix-clone-spidey.vercel.app/",
+  },
+  {
+    name: "Car Rental Web App using NextJS",
+    description:
+      "Fully Functional Car Rental Web App using React for frontend, Next for Server-side rendering, TailwindCSS for styling, Prisma for data Abstraction, MongoDB for storage, Next-Auth for Authentication, Typescript for scripting and Vercel for Deployment.",
+    image: "/cs.jpg",
+    github: "https://github.com/AtharvaWangikar/netflix-clone",
+    link: "https://netflix-clone-spidey.vercel.app/",
+  },
+]
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className=" md:w-1/2">
+                    <Link href={project.link}>
+                      <Image
+                        src={project.image}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                    </Link>
+                  </div>
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                      <Link href={project.link} target="_blank">
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          )
+        })}
+
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsSection
