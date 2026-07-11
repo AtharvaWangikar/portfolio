2a6acb418605cf4ac828bf25f34ea1bf60be53ff
Atharva Wangikar <90542530+AtharvaWangikar@users.noreply.github.com>
2026-07-11 17:30:08 +0530

    Update Footer.tsx
    mail updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
           </a>
 
           <a
-            href="mailto:your.atharvawangikar@gmail.com"
+            href="mailto:atharvawangikar@gmail.com"
             aria-label="Email"
           >
             <AiOutlineMail
