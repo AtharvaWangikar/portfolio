05526deb35a9d4b441a6b9dc06206f65bdeb8639
Atharva Wangikar <90542530+AtharvaWangikar@users.noreply.github.com>
2026-07-11 17:51:11 +0530

    Update page.tsx
    Prof

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import AboutSection from "@/components/AboutSection"
 import HeroSection from "@/components/HeroSection"
 import ProjectsSection from "@/components/ProjectsSection"
 import WorkEx from "@/components/WorkEx"
+import ProfessionalExperience from "@/components/ProfessionalExperience"
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
       <HeroSection />
       <AboutSection />
-      <WorkEx/>
-      <ProjectsSection />
+      /*<WorkEx/>
+      <ProjectsSection />*/
+      <ProfessionalExperience/>
     </main>
   )
 }
